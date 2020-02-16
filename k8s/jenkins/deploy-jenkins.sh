7e2a65205f52e02900ea39bc29f96a509742294e
Mateusz Nowakowski <mateusz.nowakowski@gmail.com>
2020-02-16 23:17:22 +0100

    replace deploy-jenkins script with Ansible playbook, expose Jenkins via https ingress

--- k8s/jenkins/deploy-jenkins.sh
+++ k8s/jenkins/deploy-jenkins.sh
@@ -1,16 +1,48 @@
 #!/usr/bin/env bash
-[ -x /usr/local/bin/helm ] || (\
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash; \
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/)
 
-kubectl create ns ci
-kubectl config set-context --current --namespace=ci
-helm install ci stable/jenkins -f configuration.yaml
+function ensurePlaybookRequirements(){
+  [ -x /usr/local/bin/helm ] || (
+    curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+    helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+  )
+  pip3 show openshift 1>/dev/null || pip3 install openshift
+}
+
+function usage(){
+echo -e "Usage: `basename $0` -e|--env minikube/gke [env]
+
+Deploys Jenkins in 'env'.
+Assumes kubectl is logged to 'env' cluster already.
+
+Samples:
+# deploy to minikube
+`basename $0` -e minikube
+or
+`basename $0` minikube
+
+# deploy to gke
+`basename $0` -e gke
+"    
+}
+
+ensurePlaybookRequirements
 
 
-while [ "`kubectl get svc ci-jenkins -n ci -o jsonpath="{.status..ip}" 2>/dev/null | xargs`" == "" ]; do
-  echo "Waiting for LoadBalancer for Jenkins..."; sleep 1
-done
+while [[ "$#" -gt 0 ]]; do case $1 in
+  -h|--help|help) usage; exit 1;;
+  -e|--env) ENV="$2"; shift;;
+  *) PARAMS+=("$1");; # save it in an array for later
+esac; shift; done
+set -- "${PARAMS[@]}" # restore positional parameters
 
-echo "Jenkins Url: http://$(kubectl get svc ci-jenkins -n ci -o jsonpath="{.status..ip}")"
-echo "User: admin Password: $(kubectl get secret --namespace ci ci-jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode)"
+ENV=${ENV:-${PARAMS[0]}}
+
+if [[ -z "$ENV" ]]; then 
+  usage
+  exit 1
+fi 
+
+ansible-playbook deploy-jenkins.yaml -v -e env=${ENV} && (
+  echo "Jenkins Url: https://$(kubectl get ingress ci-jenkins -n ci -o jsonpath="{.spec.rules[0].host}")"
+  echo "User: admin Password: $(kubectl get secret --namespace ci ci-jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 -d)"
+)
