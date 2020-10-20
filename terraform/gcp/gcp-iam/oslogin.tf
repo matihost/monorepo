cf021ec7ea242e2b333e08ea0bd9238e9a9a98b6
Mateusz Nowakowski <mateusz.nowakowski@gmail.com>
2020-10-20 21:37:34 +0200

    fix os_login resource after fixing Terraform https://github.com/hashicorp/terraform-provider-google/issues/7420

--- terraform/gcp/gcp-iam/oslogin.tf
+++ terraform/gcp/gcp-iam/oslogin.tf
@@ -9,8 +9,9 @@ data "google_client_openid_userinfo" "me" {
 
 
 resource "google_os_login_ssh_public_key" "login" {
-  user = data.google_client_openid_userinfo.me.email
-  key  = file("~/.ssh/id_rsa.cloud.vm.pub")
+  user    = data.google_client_openid_userinfo.me.email
+  key     = file("~/.ssh/id_rsa.cloud.vm.pub")
+  project = var.project
 }
 
 
