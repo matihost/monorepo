854d4ee67a714da91ef9e207d0df286909795095
Mateusz Nowakowski <mateusz.nowakowski@gmail.com>
2021-04-05 23:42:32 +0200

    Jenkins ensure job can update job if already present

--- k8s/jenkins/roles/setup_job/files/ensure-job.template.groovy
+++ k8s/jenkins/roles/setup_job/files/ensure-job.template.groovy
@@ -1,9 +1,19 @@
 
 import jenkins.model.*
+import javax.xml.transform.stream.*
 
 def jobName = "{{ job.name }}"
 def configXml = """{{ job_definition }}"""
 
 def xmlStream = new ByteArrayInputStream( configXml.toString().getBytes("UTF-8") )
 
-Jenkins.instance.createProjectFromXML(jobName, xmlStream)
+def job = Jenkins.getInstance().getItemByFullName(jobName)
+
+if (job == null) {
+  println "Constructing job ${jobName}"
+  Jenkins.instance.createProjectFromXML(jobName, xmlStream)
+}
+else {
+  println "Updating job ${jobName}"
+  job.updateByXml(new StreamSource(xmlStream));
+}
