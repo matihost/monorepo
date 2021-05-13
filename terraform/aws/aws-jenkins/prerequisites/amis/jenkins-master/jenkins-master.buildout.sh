d2e71c66e9deb5c5461b86b4f9293091452b4c94
Mateusz Nowakowski <mateusz.nowakowski@gmail.com>
2021-05-13 11:22:43 +0200

    update Jenkins to the latest lts and all dependant plugins

--- terraform/aws/aws-jenkins/prerequisites/amis/jenkins-master/jenkins-master.buildout.sh
+++ terraform/aws/aws-jenkins/prerequisites/amis/jenkins-master/jenkins-master.buildout.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 set -x
-export JENKINS_PLUGIN_MANAGER_VERSION="2.9.0"
+export JENKINS_PLUGIN_MANAGER_VERSION="2.9.2"
 export JENKINS_PLUGINS="ec2 \
   workflow-job:2.40 \
   workflow-aggregator:2.6 \
+  cloudbees-disk-usage-simple:0.10
+  credentials:2.4.1 \
   credentials-binding:1.24 \
-  git:4.7.0 \
-  configuration-as-code:1.47 \
-  timestamper:1.12 \
-  github-branch-source:2.10.2 \
+  git:4.7.1 \
+  configuration-as-code:1.51 \
+  timestamper:1.13 \
+  github-branch-source:2.10.4 \
   github-oauth:0.33 \
-  matrix-auth:2.6.6 \
-  prometheus:2.0.8 \
+  matrix-auth:2.6.7 \
+  prometheus:2.0.10 \
   simple-theme-plugin:0.6 \
   jdk-tool:1.5 \
-  command-launcher:1.5 \
+  command-launcher:1.6 \
   jaxb:2.3.0.1 \
-  branch-api:2.6.3"
+  branch-api:2.6.4"
 
 function download_jenkins_plugin_manager_cli() {
   sudo curl -sSL "https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/${JENKINS_PLUGIN_MANAGER_VERSION}/jenkins-plugin-manager-${JENKINS_PLUGIN_MANAGER_VERSION}.jar" -o /usr/share/jenkins/jenkins-plugin-manager.jar
@@ -26,7 +28,7 @@ function download_jenkins_plugins() {
   # download them to intermediate location /usr/share/jenkins/ref/plugins
   # from there they need to be copied to /var/lib/jenkins/plugins during actual Jenkins EC2 startup
   # shellcheck disable=SC2086
-  sudo java -jar /usr/share/jenkins/jenkins-plugin-manager.jar --plugins ${JENKINS_PLUGINS}
+  sudo java -jar /usr/share/jenkins/jenkins-plugin-manager.jar --latest true --plugins ${JENKINS_PLUGINS}
 }
 
 # Main
