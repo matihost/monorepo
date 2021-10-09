76b2908792086d080b65801a14b6908ba0b57f46
Mateusz Nowakowski <mateusz.nowakowski@gmail.com>
2021-10-09 21:30:34 +0200

    update Jenkins 2.303.2 along with Helm and plugins

--- terraform/aws/aws-jenkins/prerequisites/amis/jenkins-master/jenkins-master.buildout.sh
+++ terraform/aws/aws-jenkins/prerequisites/amis/jenkins-master/jenkins-master.buildout.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 set -x
-export JENKINS_PLUGIN_MANAGER_VERSION="2.10.0"
+export JENKINS_PLUGIN_MANAGER_VERSION="2.11.0"
 export JENKINS_PLUGINS="ec2 \
   workflow-job:2.41 \
   workflow-aggregator:2.6 \
   cloudbees-disk-usage-simple:0.10
-  credentials:2.5 \
+  credentials:2.6.1 \
   credentials-binding:1.27 \
-  git:4.7.2 \
-  configuration-as-code:1.51 \
+  git:4.9.0 \
+  configuration-as-code:1.54 \
   timestamper:1.13 \
-  github-branch-source:2.11.1 \
-  github-oauth:0.33 \
-  matrix-auth:2.6.7 \
+  github-branch-source:2.11.3 \
+  github-oauth:0.34 \
+  matrix-auth:2.6.8 \
   prometheus:2.0.10 \
   simple-theme-plugin:0.7 \
   jdk-tool:1.5 \
   command-launcher:1.6 \
   jaxb:2.3.0.1 \
-  branch-api:2.6.5"
+  branch-api:2.7.0"
 
 function download_jenkins_plugin_manager_cli() {
   sudo curl -sSL "https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/${JENKINS_PLUGIN_MANAGER_VERSION}/jenkins-plugin-manager-${JENKINS_PLUGIN_MANAGER_VERSION}.jar" -o /usr/share/jenkins/jenkins-plugin-manager.jar
