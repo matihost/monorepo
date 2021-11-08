4a26f2f96b7ccc9e5cb83136fe29394e8682483d
Mateusz Nowakowski <mateusz.nowakowski@gmail.com>
2021-11-08 23:10:05 +0100

    tune java module config for Intellij Idea and VS Code

--- java/apps/command-line/src/test/java/module-info.java
+++ java/apps/command-line/src/test/java/module-info.java
@@ -1,13 +1,23 @@
 open module org.matihost.learning.apps.commandline.test {
   requires org.matihost.learning.apps.commandline;
 
-  requires transitive spring.boot.test;
+  // spring boot test
+  requires spring.boot.test;
+  requires spring.boot;
+  requires spring.test;
+
+  // junit4
+  requires junit;
+  // junit5
+  requires transitive org.junit.jupiter.engine;
+  requires transitive org.junit.jupiter.api;
+  requires transitive org.junit.jupiter.params;
+
+  requires org.mockito;
+  requires org.mockito.junit.jupiter;
+
   requires transitive org.assertj.core;
   // to mitigate https://github.com/mockito/mockito/issues/2282
   requires transitive net.bytebuddy;
 
-  // junit
-  requires transitive org.junit.jupiter.engine;
-  requires transitive org.junit.jupiter.api;
-  requires transitive org.junit.jupiter.params;
 }
