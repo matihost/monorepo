43a5737058aae67996eb2a3df19906ee00e2c8fa
Mateusz Nowakowski <mateusz.nowakowski@gmail.com>
2023-02-11 22:53:47 +0100

    add packer to linux ansible playbook, update jenkins, update destroy okd4 script

--- k8s/okd/okd4/destroy-cluster.sh
+++ k8s/okd/okd4/destroy-cluster.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
+# https://github.com/openshift/installer/issues/4232
+# the only way to pass google credentials is via GSA key file path
+export GOOGLE_CREDENTIALS="files/gcp-prerequisites/target/key.json"
+
+function ensure_gsa_key_present() {
+  [ -e "${GOOGLE_CREDENTIALS}" ] || (
+    cd files/gcp-prerequisites &&
+      mkdir -p target &&
+      make run &&
+      make use-okd-installer-sa &&
+      make get-okd-installer-sa-key >target/key.json
+  )
+}
+
 function usage() {
-  echo -e "Usage: $(basename "$0") -n | --name cluster-name -k|--key-file /path/to/gsa/key.json
+  echo -e "Usage: $(basename "$0") -n | --name cluster-name
 
 Destroy OKD cluster in GCP.
 
 Samples:
-$(basename "$0") -n okd -k /path/to/gsa/key.json
+$(basename "$0") -n okd
 or
-$(basename "$0") okd /path/to/gsa/key.json
+$(basename "$0") okd
 "
 }
 
@@ -22,10 +36,6 @@ while [[ "$#" -gt 0 ]]; do
     CLUSTER_NAME="$2"
     shift
     ;;
-  -k | --key-file)
-    KEY_FILE="$2"
-    shift
-    ;;
   *) PARAMS+=("$1") ;; # save it in an array for later
   esac
   shift
@@ -33,19 +43,14 @@ done
 set -- "${PARAMS[@]}" # restore positional parameters
 
 CLUSTER_NAME=${ENV:-${PARAMS[0]}}
-KEY_FILE=${KEY_FILE:-${PARAMS[1]}}
 
-if [[ -z "$CLUSTER_NAME" || -z "$KEY_FILE" ]]; then
+if [[ -z "$CLUSTER_NAME" ]]; then
   usage
   exit 1
 fi
 
 # Main
-
-# https://github.com/openshift/installer/issues/4232
-# the only way to pass google credentials is via GSA key file path
-export GOOGLE_CREDENTIALS="${KEY_FILE}"
-
+ensure_gsa_key_present
 CLUSTER_CONFIG_DIR="target/${CLUSTER_NAME}"
 
 openshift-install destroy cluster --dir="${CLUSTER_CONFIG_DIR}" --log-level debug
