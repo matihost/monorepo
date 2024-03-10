f2b9d23c62c6391a384fb4f75bbd10f4d0ff7903
Mateusz Nowakowski <mateusz.nowakowski@gmail.com>
2024-03-10 20:30:46 +0100

    feat: mirate aws-instance to terragrunt

--- terraform/aws/aws-instance/module/ec2.tf
+++ terraform/aws/aws-instance/module/ec2.tf
@@ -1,19 +1,28 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-
-resource "random_id" "instance_id" {
-  byte_length = 8
-}
-
 data "aws_vpc" "default" {
-  default = true
+  default = var.vpc == "default" ? true : null
+
+  tags = var.vpc == "default" ? null :  {
+    Name = var.vpc
+  }
+}
+
+data "aws_subnet" "public_zone" {
+  vpc_id            = data.aws_vpc.default.id
+  availability_zone = var.zone
+
+  default_for_az = var.subnet == "default" ? true : null
+
+  tags = var.subnet == "default" ? null :  {
+    Tier = var.subnet
+  }
+
 }
 
 resource "aws_security_group" "private_access" {
-  name        = "private_access"
+  name        = "${local.prefix}-${var.region}-${var.name}"
   description = "Allow HTTP access from single computer or VPC and opens SSH"
+
+  vpc_id = data.aws_vpc.default.id
 
   tags = {
     Name = "private_access"
@@ -53,22 +62,29 @@ resource "aws_security_group" "private_access" {
 
 
 resource "aws_key_pair" "vm_key" {
-  key_name   = "vm"
-  public_key = file("~/.ssh/id_rsa.aws.vm.pub")
+  key_name   = "${local.prefix}-${var.region}-${var.name}"
+  public_key = var.ssh_pub_key
 }
 
 
 data "aws_ami" "ubuntu" {
   most_recent = true
 
+  # possible filter ids from sample image:
+  # aws ec2 describe-images --region us-east-1 --image-ids ami-0fc5d935ebf8bc3bc
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-*-server-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = [var.ec2_architecture]
   }
 
   owners = ["099720109477"] # Canonical
@@ -77,25 +93,27 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "vm" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t4g.small"
+  instance_type          = var.ec2_instance_type
   key_name               = aws_key_pair.vm_key.key_name
   vpc_security_group_ids = [aws_security_group.private_access.id]
+  subnet_id              = data.aws_subnet.public_zone.id
+
   iam_instance_profile   = var.instance_profile
   # to use cloud-init and bash script
   # use https://registry.terraform.io/providers/hashicorp/template/latest/docs/data-sources/cloudinit_config
-  user_data = templatefile("ec2.cloud-init.tpl", {
-    ssh_key = filebase64("~/.ssh/id_rsa.aws.vm"),
-    ssh_pub = filebase64("~/.ssh/id_rsa.aws.vm.pub"),
+  user_data = templatefile("${path.module}/ec2.cloud-init.tpl", {
+    ssh_key = base64encode(var.ssh_key),
+    ssh_pub = base64encode(var.ssh_pub_key),
     }
   )
   tags = {
-    Name = "vm-${random_id.instance_id.hex}"
+    Name = "${local.prefix}-${var.region}-${var.name}"
   }
 
   connection {
     type        = "ssh"
     user        = "ubuntu" # Ubuntu AMI has ubuntu user name instead of ec2-user
-    private_key = file("~/.ssh/id_rsa.aws.vm")
+    private_key = var.ssh_key
     host        = self.public_ip
   }
 
@@ -108,17 +126,10 @@ resource "aws_instance" "vm" {
   }
 }
 
-variable "external_access_ip" {
-  type        = string
-  description = "The public IP which is allowed to access instance"
-}
 
-variable "instance_profile" {
-  default     = ""
-  type        = string
-  description = "The name of instance_profile (dynamically provisioning access to role)"
+output "ec2_id" {
+  value = aws_instance.vm.id
 }
-
 
 output "ec2_ip" {
   value = aws_instance.vm.public_ip
---
+++ terraform/aws/aws-instance/module/variables.tf
@@ -0,0 +1,86 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+  # tflint-ignore: terraform_unused_declarations
+  account_id = data.aws_caller_identity.current.account_id
+
+  prefix = var.env
+}
+
+variable "env" {
+  type        = string
+  description = "Environment name"
+  default = "dev"
+}
+
+
+variable "name" {
+  type        = string
+  description = "EC2 name"
+  default     = "vm"
+}
+
+variable "ssh_pub_key" {
+  type        = string
+  description = "The pem encoded SSH pub key for accessing VMs"
+}
+
+variable "ssh_key" {
+  type        = string
+  description = "The pem encoded SSH priv key to place on bastion"
+}
+
+variable "vpc" {
+  type        = string
+  description = "VPC name where to place VM, when 'default' value, default VPC is used "
+  default     = "default"
+}
+
+variable "subnet" {
+  type        = string
+  description = "Subnet name where to place VM, when 'default' value, default subnet for zone is used, otherwise Tier tag name is used"
+  default     = "default"
+}
+
+
+variable "ec2_instance_type" {
+  type        = string
+  description = "Instance type for EC2 deployments"
+  default = "t3.micro"
+}
+
+variable "ec2_architecture" {
+  type        = string
+  description = "Instance type for EC2 deployments"
+  default = "x86_64"
+}
+
+variable "zone" {
+  default     = "us-east-1a"
+  type        = string
+  description = "Preffered AWS AZ where resources need to placed, has to be compatible with region variable"
+}
+
+variable "region" {
+  default     = "us-east-1"
+  type        = string
+  description = "Preffered AWS region where resource need to be placed"
+}
+
+# tflint-ignore: terraform_unused_declarations
+variable "aws_tags" {
+  type = map(string)
+  description = "AWS tags"
+}
+
+variable "instance_profile" {
+  default     = ""
+  type        = string
+  description = "The name of instance_profile (dynamically provisioning access to role)"
+}
+
+
+variable "external_access_ip" {
+  type        = string
+  description = "The public IP which is allowed to access instance"
+}
