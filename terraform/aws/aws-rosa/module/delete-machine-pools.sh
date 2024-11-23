06532d52bb9078bf289516c857b480360ea5c37e
Mateusz Nowakowski <mateusz.nowakowski@gmail.com>
2024-11-23 22:50:41 +0100

    feat: add waiting on machine pool removal

--- terraform/aws/aws-rosa/module/delete-machine-pools.sh
+++ terraform/aws/aws-rosa/module/delete-machine-pools.sh
@@ -17,3 +17,17 @@ for pool in $MACHINE_POOLS; do
     rosa delete machinepool -c "${CLUSTER_ID}" "${pool}" -y
   fi
 done
+
+max_wait=$((30 * 60))
+elapsed=0
+interval=10
+
+while [[ "$(rosa list machinepools -c "${CLUSTER_ID}" -o json | jq -r '.[].id' | grep -c "^${MACHINE_POOL_PREFIX}")" -ne 0 ]]; do
+  echo "Waiting for ${MACHINE_POOL_PREFIX} to disappear... ($elapsed seconds elapsed)"
+  sleep "${interval}"
+  elapsed=$((elapsed + interval))
+  if ((elapsed >= max_wait)); then
+    echo "Timeout reached after 30 minutes... ${MACHINE_POOL_PREFIX} still exist in the cluster "
+    exit 1
+  fi
+done
